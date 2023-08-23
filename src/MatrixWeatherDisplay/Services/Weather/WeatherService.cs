4d44eb1e7cb5975f671d702432f77e58c4635d01
Yannik Höflich <ygidyyy@gmail.com>
2023-08-23 18:57:58 +0200

    Added: Auto select enabled weather service

--- src/MatrixWeatherDisplay/Services/Weather/WeatherService.cs
+++ src/MatrixWeatherDisplay/Services/Weather/WeatherService.cs
@@ -3,12 +3,12 @@ using MatrixWeb.Extensions;
 using MatrixWeb.Extensions.Services;
 
 namespace MatrixWeatherDisplay.Services.Weather;
-public class WeatherService : IEnableable, IService {
+public class WeatherService : IService, IInitializable {
     private readonly Dictionary<WeatherProvider, CachedWeatherClient> _clients;
 
     public WeatherProvider WeatherProvider { get; set; } = WeatherProvider.OpenWeatherMap;
 
-    public bool IsEnabled => _clients[WeatherProvider].IsEnabled;
+    public bool IsEnabled => _clients.Any(c => c.Value.IsEnabled);
 
     public WeatherService(OpenWeatherMapClient openWeatherMapClient, WeatherApiClient weatherApiClient) {
         _clients = new() {
@@ -17,10 +17,21 @@ public class WeatherService : IEnableable, IService {
         };
     }
 
+    public void Init() {
+        WeatherProvider = _clients.FirstOrDefault(x => x.Value.IsEnabled).Key;
+    }
+
     public async Task<WeatherStatus> GetWeatherAsync() {
         CachedWeatherClient provider = _clients[WeatherProvider];
+
+        if (!provider.IsEnabled) {
+            return new WeatherStatus();
+        }
+
         return await provider.GetWeatherAsync();
     }
+
+    public bool IsProviderEnabled(WeatherProvider weatherProvider) => _clients[weatherProvider].IsEnabled;
 }
 
 public enum WeatherProvider{
